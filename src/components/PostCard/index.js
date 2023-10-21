eaaa2b784f8d7d8cdc5669dc6baa33a1b4cb9d77
color.ui <ragipavanchary@gmail.com>
2023-10-21 06:36:48 +0000

    day two

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -8,32 +8,6 @@ import {FaRegComment} from 'react-icons/fa'
 
 import {BiShareAlt} from 'react-icons/bi'
 
-const RawData = {
-  caption: 'Another day, another sunrise',
-  createdAt: '4 Hours Ago',
-  imageUrl:
-    'https://assets.ccbp.in/frontend/react-js/instagram-mini-project/posts/instagram-mini-project-post-1-img.png',
-  likesCount: 7,
-  postId: 'f25d77f0-602e-41d1-971e-4b8cf54709eb',
-  profilePic:
-    'https://assets.ccbp.in/frontend/react-js/instagram-mini-project/users/instagram-mini-project-user-1-img.png',
-  userId: 'Varun_Aadithya',
-
-  userName: 'Varun Aadithya',
-  comments: [
-    {
-      user_name: 'Prabuddha Dasgupta',
-      user_id: 'Prabuddha_Dasgupta',
-      comment: 'Lightning is incredible.',
-    },
-
-    {
-      user_name: 'Gautam Rajadhyaksha',
-      user_id: 'Gautam_Rajadhyaksha',
-      comment: 'The Earth laughs in flowers.',
-    },
-  ],
-}
 
 class PostCard extends Component {
   render() {
